b5c811a68f6476e5d77d7f5a7200a577f56ce709
AnanthFrancis <ananth.francis@axa.com.sg>
2019-06-22 23:24:12 +0800

    added npmrc

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,19 @@ router.get('/', function(req, res, next) {
 	  	}
   	});
 });
+
+// Retrieve user with id 
+router.get('/:id', function (req, res) {
+	let user_id = req.params.id;
+	if (!user_id) {
+	 return res.status(400).send({ error: true, message: 'Please provide user_id' });
+	}
+	connection.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
+	 if (error) throw error;
+	  return res.send({ error: false, data: results[0], message: 'users list.' });
+	});
+});
+
 // Add a new user  
 router.post('/', function (req, res) {
 	let id = req.body.id;
